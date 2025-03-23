3927e735ebe933ce3e67d5abe17395698f1708c3
ChristianNyamekye <christiankingnyamekye@gmail.com>
2025-03-23 13:04:11 -0400

    mongooe calls updated to use promises

--- src/services/passport.js
+++ src/services/passport.js
@@ -7,62 +7,61 @@ import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 import User from '../models/user_model';
 
 // options for local strategy, we'll use email AS the username
-// not have separate ones
 const localOptions = { usernameField: 'username' };
 
 // options for jwt strategy
-// we'll pass in the jwt in an `authorization` header
-// so passport can find it there
 const jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-  secretOrKey: process.env.AUTH_SECRET,
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey: process.env.AUTH_SECRET,
 };
-// NOTE: we are not calling this a bearer token (although it technically is), if you see people use Bearer in front of token on the internet you could either ignore it, use it but then you have to parse it out here as well as prepend it on the frontend.
-
 
 // username + password authentication strategy
-const localLogin = new LocalStrategy(localOptions, (username, password, done) => {
-  // Verify this email and password, call done with the user
-  // if it is the correct email and password
-  // otherwise, call done with false
-  // eslint-disable-next-line consistent-return
-  User.findOne({ username }, (err, user) => {
-    if (err) { return done(err); }
+const localLogin = new LocalStrategy(localOptions, async (username, password, done) => {
+    try {
+        // Use .exec() to return a promise instead of passing a callback directly.
+        const user = await User.findOne({ username }).exec();
 
-    if (!user) { return done(null, false); }
+        if (!user) {
+            return done(null, false);
+        }
 
-    // compare passwords - is `password` equal to user.password?
-    user.comparePassword(password, (err, isMatch) => {
-      if (err) {
-        done(err);
-      } else if (!isMatch) {
-        done(null, false);
-      } else {
-        done(null, user);
-      }
-    });
-  });
-});
+        // Wrap the comparePassword function in a Promise (assuming it uses a callback).
+        const isMatch = await new Promise((resolve, reject) => {
+            user.comparePassword(password, (err, match) => {
+                if (err) return reject(err);
+                resolve(match);
+            });
+        });
 
-const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
-  // See if the user ID in the payload exists in our database
-  // If it does, call 'done' with that other
-  // otherwise, call done without a user object
-  User.findById(payload.sub, (err, user) => {
-    if (err) {
-      done(err, false);
-    } else if (user) {
-      done(null, user);
-    } else {
-      done(null, false);
+        if (!isMatch) {
+            return done(null, false);
+        }
+
+        return done(null, user);
+    } catch (err) {
+        return done(err);
     }
-  });
 });
 
-// Tell passport to use this strategy
+// jwt authentication strategy
+const jwtLogin = new JwtStrategy(jwtOptions, async (payload, done) => {
+    try {
+        // Use .exec() to work with promises.
+        const user = await User.findById(payload.sub).exec();
+
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    } catch (err) {
+        return done(err, false);
+    }
+});
+
+// Tell passport to use these strategies
 passport.use(jwtLogin);
 passport.use(localLogin);
-
 
 export const requireAuth = passport.authenticate('jwt', { session: false });
 export const requireSignin = passport.authenticate('local', { session: false });
